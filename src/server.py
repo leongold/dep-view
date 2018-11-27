0f8ba32797a5d4485efb8905b21fc815899bd0f7
Leon Goldberg <lgoldber@redhat.com>
2018-11-27 21:41:56 +0200

    multiprocessing

--- src/server.py
+++ src/server.py
@@ -1,10 +1,13 @@
 
+import concurrent.futures
 import json
 
 from aiohttp import web
+app = web.Application()
 import pickledb
-import requests
 db = pickledb.load('db', True)
+import pprint
+import requests
 
 
 NPM_REGISTRY_FMT = 'https://registry.npmjs.org/{pkg}/{version}'
@@ -28,7 +31,7 @@ def _fetch_stored_deps(pkg, version):
     return None
 
 
-async def _fetch_live_metadata(pkg, version):
+def _fetch_live_metadata(pkg, version):
     response = requests.get(
         NPM_REGISTRY_FMT.format(pkg=pkg, version=version)
     )
@@ -40,37 +43,54 @@ async def _fetch_live_metadata(pkg, version):
     return response.json()
 
 
-async def _fetch_live_deps(pkg, version):
+def _fetch_live_deps(key):
     """[(pkg, version), (pkg, version), ...]"""
-    metadata = await _fetch_live_metadata(pkg, version)
+    metadata = _fetch_live_metadata(*key)
     dependencies = metadata.get('dependencies', {})
-    return [(p, v.replace('~', '')) for p, v in dependencies.items()]
+    return [(p, _clean_version(v)) for p, v in dependencies.items()]
 
 
-async def _fetch_deps(pkg, version, result):
-    version = _clean_version(version)
+def _fetch_deps(pkg, version, result):
     stored_deps = _fetch_stored_deps(pkg, version)
     if stored_deps is not None:
         deps = stored_deps
     else:
-        deps = await _fetch_live_deps(pkg, version)
+        deps = _fetch_live_deps((pkg, version))
         db.set(json.dumps((pkg, version)), deps)
 
-    result_ = {}
-    for pkg_, version_ in deps:
-        await _fetch_deps(pkg_, version_, result_)
-    result[json.dumps((pkg, version))] = result_
+    all_deps = {(p, v) for p, v in deps}
+
+    cached_deps = {(p, v) for p, v in deps if db.exists(json.dumps((p, v)))}
+    cached_deps_subdeps = {(p, v): db.get((p, v)) for p, v in cached_deps}
+
+    missing_deps = all_deps.difference(cached_deps)
+    missing_deps_subdeps = {}
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        for (pkg_, version_), subdeps in zip(
+                missing_deps, executor.map(_fetch_live_deps, missing_deps)
+            ):
+            missing_deps_subdeps[
+                (pkg_, _clean_version(version_))
+            ] = subdeps
+
+    all_deps_subdeps = {**missing_deps_subdeps, **cached_deps_subdeps}
+    for (pkg_, version_), subdeps in all_deps_subdeps.items():
+        version_ = _clean_version(version_)
+        sub_sub_deps = {}
+        result[(pkg_, version_)] = sub_sub_deps
+        _fetch_deps(pkg_, version_, sub_sub_deps)
 
 
-async def main(request):
+def main(request):
     pkg = request.match_info.get('pkg')
-    version = request.match_info.get('version')
-    result = {}
-    await _fetch_deps(pkg, version, result)
-    return web.json_response(result)
+    version = _clean_version(request.match_info.get('version'))
+
+    sub_deps = {}
+    result = {(pkg, version): sub_deps}
+    _fetch_deps(pkg, version, sub_deps)
+    return web.Response(text=pprint.pformat(result))
 
 
 if __name__ == '__main__':
-    app = web.Application()
     app.add_routes([web.get('/{pkg}/{version}', main)])
     web.run_app(app)
