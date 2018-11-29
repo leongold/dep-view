13dc5c92f640c8e2d257290b4c12234c68b1439a
Leon Goldberg <lgoldber@redhat.com>
2018-11-29 15:40:08 +0200

    fail early if run fails

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
-docker volume create --name=db_volume
-docker-compose build
-docker-compose up
+
+docker volume create --name=db_volume || { echo 'failed to create volume'; exit 1; }
+docker-compose build || { echo 'failed to build'; exit 1; }
+docker-compose up || { echo 'failed to run'; exit 1; }
