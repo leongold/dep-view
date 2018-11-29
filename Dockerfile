ea18c75397abf71b83a9eee9d0585ecf2bf971a7
Leon Goldberg <lgoldber@redhat.com>
2018-11-29 13:51:32 +0200

    using nginx

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,6 @@ FROM python:3.6-alpine
 ADD . /workdir
 WORKDIR /workdir
 RUN pip install -r requirements.txt
+EXPOSE 8081
 CMD ["python", "main.py"]
 
--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 
+import asyncio
 from aiohttp import web
 
 from dep_view import fetch_deps
 
 
-def on_get(request):
+async def on_get(request):
     pkg = request.match_info.get('pkg')
     version = request.match_info.get('version')
-    return web.json_response(fetch_deps(pkg, version))
+    loop = asyncio.get_event_loop()
+    result = await loop.run_in_executor(None, fetch_deps, pkg, version)
+    return web.json_response(result)
 
 
 if __name__ == '__main__':
@@ -16,4 +19,4 @@ if __name__ == '__main__':
         [web.get('/{pkg}/{version}', on_get),
          web.get('/ping', lambda _: web.json_response('pong'))]
     )
-    web.run_app(app)
+    web.run_app(app, port=8081)
