0dd96766733003ed22790e008552188d2a423052
Leon Goldberg <lgoldber@redhat.com>
2018-12-01 20:42:29 +0200

    Revert "separated scripts"
    This reverts commit 071543c3ccadbf3a453e92802ee42271e2ce0629.

--- create_volumes.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-docker volume create --name=db_volume_a_i || { echo 'failed to create volume a-i'; exit 1; }
-docker volume create --name=db_volume_j_q || { echo 'failed to create volume j-q'; exit 1; }
-docker volume create --name=db_volume_r_z || { echo 'failed to create volume r-z'; exit 1; }
-
